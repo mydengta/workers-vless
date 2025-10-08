de6ab6d888bec194745a448bfe0e8119e787a030
mydengta <165262074+mydengta@users.noreply.github.com>
2025-10-08 10:43:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import {
 
 export default {
 	async fetch(req, env) {
-		const UUID = env.UUID || 'ef9d104e-ca0e-4202-ba4b-a0afb969c747';
+		const UUID = env.UUID || 'aa2b53cf-1272-4db3-9537-3001b7689e5d';
 
 		if (req.headers.get('Upgrade')?.toLowerCase() === 'websocket') {
 			const [client, ws] = Object.values(new WebSocketPair());
